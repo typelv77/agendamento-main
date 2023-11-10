70a1bc6438d28b85d4de210b4bbfca67528ee970
typelv77 <typelvsenai@gmail.com>
2023-11-09 21:50:23 -0300

    feat: add style page partial login

--- src/components/BoxWhiteLogin/index.tsx
+++ src/components/BoxWhiteLogin/index.tsx
@@ -1,7 +1,15 @@
 import "./style.css";
 
 const BoxWhiteLogin = () => {
-  return <div id="boxWhiteLogin"></div>;
+  return <div id="boxWhiteLogin">
+    <h1 id="title-app">Agendamento de Ambientes Educacionais</h1>
+    <h2 id="title-login">Login</h2>
+    <div id="boxWhiteLogin-gradient">
+      <img id="art" src="public\art.png" alt="moça com pc olhando calendario" />
+    </div>
+    <img id="art-mobile" src="public\art-mobile.png" alt="moça com pc olhando calendario" />
+    <img id="logo-senai" src="public\senai.png" alt="moça com pc olhando calendario" />
+  </div>;
 };
 
 export default BoxWhiteLogin;
