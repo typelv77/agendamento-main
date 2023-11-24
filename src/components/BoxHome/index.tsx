63d76db39394d33131c3506547fcf3cca2020b05
typelv77 <typelvsenai@gmail.com>
2023-11-23 21:50:03 -0300

    feat: add style menul de configuração

--- src/components/BoxHome/index.tsx
+++ src/components/BoxHome/index.tsx
@@ -3,7 +3,7 @@ import "./style.css"
 const BoxHome = () => {
     return <div id="Box-principal">
         <div id="box-1">
-        <h1 id="titulo">Ambientes</h1>
+            <h1 id="titulo">Ambientes</h1>
         </div>
         <div id="box-ll1"></div>
         <div id="box-ll2"></div>
@@ -13,7 +13,24 @@ const BoxHome = () => {
         <div id="box-ll6"></div>
         <input id="input" type="" />
         <img id="img" src="public\lupa.png" alt="lupa" />
-        
+        <img id="barras" src="public\barras.png" alt="barras de opiçoes" />
+        <img id="next-button" src="public\next.png" alt="next" />
+        <span id="span" ></span>
+        <div id="menul">
+            <span className="list-modal">
+                <img id="config" src="public\configuração.png" alt="configs" />
+                <h1 className="list-modal" id="h1-configuracao">Configuração</h1>
+            </span>
+            <span className="list-modal">
+                <img id="suport" src="public\icone-suporte.png" alt="sup" />
+                <h1 className="list-modal" id="h2-suporte">Suporte</h1>
+
+            </span>
+            <span className="list-modal">
+                <img id="saida" src="public\icone-saida.png" alt="exit" />
+                <h1 className="list-modal" id="h3-sair">Sair</h1>
+            </span>
+        </div>
     </div>;
 }
 export default BoxHome;
